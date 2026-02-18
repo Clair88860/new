e3740dcfc115c8d3a2e67c9264e7cb099366e307
claire <melanie.rossek@gmail.com>
2026-02-18 10:16:09 +0100

    Update BLEScanCallback.java

--- java/org/example/BLEScanCallback.java
+++ java/org/example/BLEScanCallback.java
@@ -5,15 +5,28 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.util.Log;
 
+import org.jnius.NativeInvocation;
+
 public class BLEScanCallback extends ScanCallback {
 
+    private final Object pyCallback;
     private final String TAG = "BLEScanCallback";
+
+    public BLEScanCallback(Object pyCallback) {
+        this.pyCallback = pyCallback;
+    }
 
     @Override
     public void onScanResult(int callbackType, ScanResult result) {
         BluetoothDevice device = result.getDevice();
         String name = device.getName();
         Log.d(TAG, "Gefunden: " + name);
-        // TODO: Hier kannst du das Gerät an Python weitergeben (z.B. via Broadcast oder Pyjnius)
+        if (pyCallback != null) {
+            try {
+                NativeInvocation.invoke(pyCallback, "onDeviceFound", new Object[]{name}, new Class[]{String.class});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
