74d04a8e08f84b17d4e0ce356cbaedcdc9c067fb
claire <melanie.rossek@gmail.com>
2026-02-18 11:30:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from jnius import autoclass
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.utils import platform
 
 if platform == "android":
+    from jnius import autoclass
+    from android.permissions import request_permissions, Permission
+
     PythonActivity = autoclass("org.kivy.android.PythonActivity")
     BluetoothAdapter = autoclass("android.bluetooth.BluetoothAdapter")
     BLEBridge = autoclass("org.example.BLEBridge")
@@ -14,6 +16,12 @@ class BLEApp(App):
     def build(self):
         self.label = Label(text="Scan läuft..")
         if platform == "android":
+            # Runtime Permissions für Android 12+
+            request_permissions([
+                Permission.BLUETOOTH_SCAN,
+                Permission.BLUETOOTH_CONNECT,
+                Permission.ACCESS_FINE_LOCATION
+            ])
             self.adapter = BluetoothAdapter.getDefaultAdapter()
             self.bridge = BLEBridge()
             self.scan_cb = BLEScanCallback(self)
@@ -21,6 +29,7 @@ class BLEApp(App):
         return self.label
 
     def onDeviceFound(self, name):
+        # UI Update im Hauptthread
         Clock.schedule_once(lambda dt: self.label.setter('text')(self.label, f"Gefunden: {name}"))
 
     def on_stop(self):
